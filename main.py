dada71eca300277fe60a66cd2b46ae11137db320
umatbro <umatbroo@gmail.com>
2019-11-02 18:01:53 +0100

    Add sanic (#1)
    * init sanic
    
    * Use env file
    
    * Add port to config
    
    * Add more env variables

---
+++ main.py
@@ -0,0 +1,31 @@
+from sanic import Sanic
+from sanic.request import Request
+from sanic.response import HTTPResponse
+from sanic.response import json
+from dotenv import load_dotenv
+from envparse import Env
+
+load_dotenv()
+env = Env(
+    HOST=dict(cast=str, default='127.0.0.1'),
+    PORT=dict(cast=int, default=8000),
+    DEBUG=dict(cast=bool, default=False),
+    ACCESS_LOG=dict(cast=bool, default=False),
+)
+
+app = Sanic()
+app.config.update(
+    {key: env(key) for key in env.schema.keys()}
+)
+
+
+@app.route('/')
+async def index(request: Request) -> HTTPResponse:
+    return json({'hello': 'world'})
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=app.config.PORT,
+    )
