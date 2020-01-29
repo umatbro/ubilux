23a7b7b3d8c0d917ecaa8ef38d244af9f1b1fdec
umatbro <umatbroo@gmail.com>
2020-01-30 00:37:09 +0100

    setuping docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM python:3.7-alpine
 
+ARG environment=production
 ENV WORKDIR /app
 
 WORKDIR ${WORKDIR}
-RUN pip install poetry && poetry config settings.virtualenvs.create false
-RUN apk add --no-cache --virtual .build-deps build-base gcc musl-dev
+RUN apk add --no-cache --virtual .build-deps build-base gcc musl-dev libffi-dev libressl-dev
+RUN pip install poetry && poetry config virtualenvs.create false
 COPY . ${WORKDIR}
-RUN poetry install --no-dev && \
+RUN if [ "${environment}" = "production" ] ; then poetry install --no-dev ; else poetry install ; fi && \
     apk --purge del .build-deps && \
     rm ${WORKDIR}/poetry.lock ${WORKDIR}/pyproject.toml
+
+EXPOSE 80
+EXPOSE 443
 
 CMD ["python", "main.py"]
--- add_envs_to_appengine_config.py
+++ add_envs_to_appengine_config.py
@@ -7,6 +7,8 @@ import os
 import pathlib
 import sys
 
+import dotenv
+
 import yaml
 
 parser = argparse.ArgumentParser(
@@ -21,12 +23,16 @@ UPDATE_ENV_VARS = {
     "REDIS_HOST",
     "REDIS_PORT",
     "REDIS_DB",
+    "REDIS_PASSWORD",
     "DEBUG",
 }
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    env_file_path = (pathlib.Path(".") / ".env").absolute()
+    dotenv.load_dotenv(dotenv_path=env_file_path, verbose=True)
+
     file_path = pathlib.Path(args.file_path).absolute()
     if not pathlib.Path(file_path).is_file():
         print(f'File "{file_path}" does not exist', file=sys.stderr)
@@ -35,10 +41,13 @@ if __name__ == "__main__":
     with open(file_path, "r") as stream:
         config = yaml.safe_load(stream)
 
+    ENV_VARS_KEY = "env_variables"
+
+    config[ENV_VARS_KEY] = config.get(ENV_VARS_KEY, None) or {}
     for key in UPDATE_ENV_VARS:
         val = os.environ.get(key, None)
         if val:
-            config["env_vars"][key] = val
+            config[ENV_VARS_KEY][key] = val
 
     with open(file_path, "w") as stream:
         yaml.safe_dump(config, stream)
