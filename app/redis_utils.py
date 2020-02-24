295c3049f972529f24a96260166c1c82fd3df350
umatbro <umatbroo@gmail.com>
2020-02-24 21:18:29 +0100

    use sanic config instead of module

--- app/redis_utils.py
+++ app/redis_utils.py
@@ -1,9 +1,22 @@
+from redis import Redis
 from sanic.log import logger
 
 import settings
-from redis import Redis
 
 
 def redis_connect() -> Redis:
+    from main import app
+
     logger.info(f'Opening redis connection with {settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}')
-    return Redis(connection_pool=settings.REDIS_POOL)
+    return Redis(
+        host=app.config.REDIS_HOST,
+        port=app.config.REDIS_PORT,
+        db=app.config.REDIS_DB,
+    )
+
+
+def check_redis_connection():
+    r = redis_connect()
+    r.ping()
+    r.close()
+    return True
