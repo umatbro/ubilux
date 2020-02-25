cce48fd047c1427d31f86dba1a3a28267bd8cc3d
umatbro <umatbroo@gmail.com>
2020-02-25 23:17:15 +0100

    Add redis password to config

--- app/redis_utils.py
+++ app/redis_utils.py
@@ -7,11 +7,14 @@ import settings
 def redis_connect() -> Redis:
     from main import app
 
-    logger.info(f'Opening redis connection with {settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}')
+    logger.info(
+        f"Opening redis connection with {settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}"
+    )
     return Redis(
         host=app.config.REDIS_HOST,
         port=app.config.REDIS_PORT,
         db=app.config.REDIS_DB,
+        password=app.config.REDIS_PASSWORD,
     )
 
 
@@ -22,8 +25,10 @@ def check_redis_connection():
     # keyspace events are required for listening status change.
     # Redis should be run with the following command:
     # redis-server --notify-keyspace-events K$$
-    keyspace_events = r.config_get('notify-keyspace-events').get('notify-keyspace-events')
-    assert '$' in keyspace_events
-    assert 'K' in keyspace_events
+    keyspace_events = r.config_get("notify-keyspace-events").get(
+        "notify-keyspace-events"
+    )
+    assert "$" in keyspace_events
+    assert "K" in keyspace_events
     r.close()
     return True
--- settings.py
+++ settings.py
@@ -5,10 +5,11 @@ from decouple import config
 BASE_DIR = os.path.dirname(__file__)
 
 
-HOST = config('HOST', default='0.0.0.0')
-PORT = config('PORT', default=8000, cast=int)
-DEBUG = config('DEBUG', default=False, cast=bool)
-ACCESS_LOG = config('ACCESS_LOG', default=False, cast=bool)
-REDIS_HOST = config('REDIS_HOST', default='127.0.0.1', cast=str)
-REDIS_PORT = config('REDIS_PORT', default=6379, cast=int)
-REDIS_DB = config('REDIS_DB', default=0, cast=int)
+HOST = config("HOST", default="0.0.0.0")
+PORT = config("PORT", default=8000, cast=int)
+DEBUG = config("DEBUG", default=False, cast=bool)
+ACCESS_LOG = config("ACCESS_LOG", default=False, cast=bool)
+REDIS_HOST = config("REDIS_HOST", default="127.0.0.1", cast=str)
+REDIS_PORT = config("REDIS_PORT", default=6379, cast=int)
+REDIS_DB = config("REDIS_DB", default=0, cast=int)
+REDIS_PASSWORD = config("REDIS_PASSWORD", default="", cast=str)
